875113b8c6d68deeb35830a01bf55514682ec545
campostalita <campos.talita@hotmail.com>
2020-07-29 14:38:57 -0300

    Slider ok, começo do formulário

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,17 +1,23 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import PageDefault from '../../../components/PageDefault'
+import React from "react";
+import { Link } from "react-router-dom";
+import PageDefault from "../../../components/PageDefault";
 
 function CadastroCategoria() {
-    return(
-     <PageDefault>
-         
-       <h1>Cadastro de Categoria</h1>
-       <Link to='/' >
-           Ir para home
-       </Link>
-     </PageDefault>
-    )
-   }
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
+      <form>
+        <label>
+          Nome da Categoria:
+          <input type="text" />
+        </label>
 
-   export default CadastroCategoria
+        <button>Cadastrar</button>
+      </form>
+
+      <Link to="/">Ir para home</Link>
+    </PageDefault>
+  );
+}
+
+export default CadastroCategoria;
